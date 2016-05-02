8b93f03fee334fd7f3afe76fdd596482ad669120
aileen santos <asantos3026@gmail.com>
2016-05-02 00:49:14 -0700

    added structure to index.html file and notes-index.html and angular module in app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+angular.module('NoteWrangler', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/notes', {
+    templateUrl: 'templates/pages/notes/index.html'
+  });
+  
+}]);
